e7ab5aca6f6b20cb15b6e4ef4aab25897562ad7b
lalit293 <lalitsingh.micro@gmail.com>
2024-08-11 16:15:39 +0530

    ♻️ refactor: made the theme toggle component better

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -2,16 +2,10 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@shadcn-components/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@shadcn-components/dropdown-menu"
 
 export function ThemeToggle() {
   const [theme, setThemeState] = React.useState<
-    "theme-light" | "dark" | "system"
+    "theme-light" | "dark"
   >("theme-light")
 
   React.useEffect(() => {
@@ -20,33 +14,20 @@ export function ThemeToggle() {
   }, [])
 
   React.useEffect(() => {
-    const isDark =
-      theme === "dark" ||
-      (theme === "system" &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    const isDark = theme === "dark"
     document.documentElement.classList[isDark ? "add" : "remove"]("dark")
   }, [theme])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="absolute right-1 top-1">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" className="absolute right-1 top-1" onClick={() => {
+      if (document.documentElement.classList.contains("dark")) {
+        setThemeState("theme-light");
+      } else {
+        setThemeState("dark")
+      }
+    }}>
+      {theme === "theme-light" ? <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 duration-300" /> :
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-180 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-300" /> }
+    </Button>
   )
 }
